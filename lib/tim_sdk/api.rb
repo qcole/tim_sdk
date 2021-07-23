53de408b0c09c2afc642f8ebaac84859ad386044
lyo1 <longzc123@163.com>
2021-07-23 13:53:04 +0800

    fix

--- lib/tim_sdk/api.rb
+++ lib/tim_sdk/api.rb
@@ -2,7 +2,6 @@ require 'faraday'
 
 module TimSdk
   class Api
-    include Api::Friendship
 
     def self.connection
       Faraday.new('https://console.tim.qq.com', params: {
@@ -273,6 +272,44 @@ module TimSdk
       response = connection.post('/v4/group_open_http_svc/destroy_group') do |request|
         request.body = {
           "GroupId": group_id
+        }.to_json
+      end
+      raise TimServerError, "Response Status: #{response.status}" unless response.success?
+      JSON.parse(response.body, symbolize_names: true) if response.success?
+    end
+
+    # 添加好友
+    def self.invoke_friend_add(from_account, friend_list)
+      response = connection.post('/v4/sns/friend_add') do |request|
+        request.body = { 
+          :From_Account => from_account,
+          :AddFriendItem => friend_list.map { |friend| { "To_Account": friend, "AddSource": "AddSource_Type_Server" }}
+        }.to_json
+      end
+      raise TimServerError, "Response Status: #{response.status}" unless response.success?
+      JSON.parse(response.body, symbolize_names: true) if response.success?
+    end
+
+    # 导入好友
+    def self.invoke_friend_import(from_account, friend_list)
+      response = connection.post('/v4/sns/friend_import') do |request|
+        request.body = { 
+          :From_Account => from_account,
+          :AddFriendItem => friend_list.map { |friend| { "To_Account": friend, "AddSource": "AddSource_Type_Server" }}
+        }.to_json
+      end
+      raise TimServerError, "Response Status: #{response.status}" unless response.success?
+      JSON.parse(response.body, symbolize_names: true) if response.success?
+    end
+
+    # 拉取好友
+    def self.invoke_friend_get(from_account, start_index = 0, standard_sequence = 0, custom_sequence = 0)
+      response = connection.post('/v4/sns/friend_get') do |request|
+        request.body = { 
+          :From_Account => from_account,
+          "StartIndex": start_index,
+          "StandardSequence": standard_sequence,
+          "CustomSequence": custom_sequence
         }.to_json
       end
       raise TimServerError, "Response Status: #{response.status}" unless response.success?
--- lib/tim_sdk/api/friendship.rb
+++ lib/tim_sdk/api/friendship.rb
@@ -1,43 +1,7 @@
 class Api
   module Friendship
 
-    # 添加好友
-    def self.invoke_friend_add(from_account, friend_list)
-      response = connection.post('/v4/sns/friend_add') do |request|
-        request.body = { 
-          :From_Account => from_account,
-          :AddFriendItem => friend_list.map { |friend| { "To_Account": friend, "AddSource": "AddSource_Type_Server" }}
-        }.to_json
-      end
-      raise TimServerError, "Response Status: #{response.status}" unless response.success?
-      JSON.parse(response.body, symbolize_names: true) if response.success?
-    end
-
-    # 导入好友
-    def self.invoke_friend_import(from_account, friend_list)
-      response = connection.post('/v4/sns/friend_import') do |request|
-        request.body = { 
-          :From_Account => from_account,
-          :AddFriendItem => friend_list.map { |friend| { "To_Account": friend, "AddSource": "AddSource_Type_Server" }}
-        }.to_json
-      end
-      raise TimServerError, "Response Status: #{response.status}" unless response.success?
-      JSON.parse(response.body, symbolize_names: true) if response.success?
-    end
-
-    # 拉取好友
-    def self.invoke_friend_get(from_account, start_index = 0, standard_sequence = 0, custom_sequence = 0)
-      response = connection.post('/v4/sns/friend_get') do |request|
-        request.body = { 
-          :From_Account => from_account,
-          "StartIndex": start_index,
-          "StandardSequence": standard_sequence,
-          "CustomSequence": custom_sequence
-        }.to_json
-      end
-      raise TimServerError, "Response Status: #{response.status}" unless response.success?
-      JSON.parse(response.body, symbolize_names: true) if response.success?
-    end
+    
 
   end
 end
